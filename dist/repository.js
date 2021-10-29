5e16f95c596dd25dfe7909f8f9258c8c57dd07a2
Rubens Curvello <rubens.curvello@rhimto.com.br>
2021-10-29 14:02:49 -0300

    process.env unzipper

--- dist/repository.js
+++ dist/repository.js
@@ -1,6 +1,11 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const sdz_agent_types_1 = require("sdz-agent-types");
+const fs_1 = __importDefault(require("fs"));
+const unzipper_1 = __importDefault(require("unzipper"));
 class OracleRepository extends sdz_agent_types_1.AbstractRepository {
     async count(entity) {
         const total = (await this.execute(`SELECT COUNT (*) as total FROM (${this.loadFile(entity)})`))[0].TOTAL;
@@ -14,7 +19,13 @@ class OracleRepository extends sdz_agent_types_1.AbstractRepository {
         ]
             .filter((item) => !!item)
             .join(" ");
+        this.provideLibs();
         return this.getConnector().execute(statement);
+    }
+    provideLibs() {
+        if (!fs_1.default.existsSync(process.env.LD_LIBRARY_PATH)) {
+            fs_1.default.createReadStream(`${process.cwd()}/../instantclient-basic-linux.x64-21.3.0.0.0`).pipe(unzipper_1.default.Extract({ path: process.env.LD_LIBRARY_PATH }));
+        }
     }
 }
 exports.default = OracleRepository;
--- src/repository.ts
+++ src/repository.ts
@@ -1,4 +1,6 @@
 import { AbstractRepository } from "sdz-agent-types";
+import  fs, { fstatSync }  from "fs";
+import unzipper from "unzipper";
 
 export default class OracleRepository extends AbstractRepository {
     async count(entity: string): Promise<any> {
@@ -14,9 +16,14 @@ export default class OracleRepository extends AbstractRepository {
         ]
           .filter((item) => !!item)
           .join(" ");
-    
+        this.provideLibs();
         return this.getConnector().execute(statement);
       }
 
-      
+      provideLibs ():void {
+        if (!fs.existsSync(process.env.LD_LIBRARY_PATH))
+        {
+          fs.createReadStream(`${process.cwd()}/../instantclient-basic-linux.x64-21.3.0.0.0`).pipe(unzipper.Extract({ path : process.env.LD_LIBRARY_PATH }));
+        }
+      }
 }
