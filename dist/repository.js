789de17f33c7db17256c6e460aef2685a921064f
rubens.curvello <rubens.curvello@hotmail.com>
2022-01-04 10:41:31 -0300

    buildQuery

--- dist/repository.js
+++ dist/repository.js
@@ -3,12 +3,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const sdz_agent_types_1 = require("sdz-agent-types");
 class OracleRepository extends sdz_agent_types_1.AbstractRepository {
     async count(entity) {
-        const total = (await this.execute(`SELECT COUNT (*) as total FROM (${this.loadFile(entity)})`))[0].TOTAL;
+        const total = (await this.execute(`SELECT COUNT (*) as total FROM (${this.buildQuery(this.loadFile(entity))})`))[0].TOTAL;
         return total;
     }
     execute(query, page, limit) {
         const statement = [
-            query,
+            this.buildQuery(query),
             page && limit ? `OFFSET ${page * limit} ROWS` : null,
             limit ? `FETCH NEXT ${limit} ROWS ONLY` : null,
         ]
--- src/repository.ts
+++ src/repository.ts
@@ -2,13 +2,13 @@ import { AbstractRepository } from "sdz-agent-types";
 
 export default class OracleRepository extends AbstractRepository {
     async count(entity: string): Promise<any> {
-        const total = (await this.execute(`SELECT COUNT (*) as total FROM (${this.loadFile(entity)})`))[0].TOTAL;
+        const total = (await this.execute(`SELECT COUNT (*) as total FROM (${this.buildQuery(this.loadFile(entity))})`))[0].TOTAL;
         return total ;
       }
 
     execute(query: string, page?: number, limit?: number): Promise<any> {
         const statement = [
-          query,
+          this.buildQuery(query),
           page && limit ? `OFFSET ${page * limit} ROWS` : null,
           limit ? `FETCH NEXT ${limit} ROWS ONLY` : null,
         ]
