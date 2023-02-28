028460b8f044fe1e275388528f9c71650607b5ec
Wander Veloso <wander.veloso@seedz.ag>
2023-02-28 13:13:13 -0300

    add build on oracle version 11g

--- dist/repository.d.ts
+++ dist/repository.d.ts
@@ -1,5 +1,7 @@
 import { AbstractRepository } from "sdz-agent-types";
 export default class OracleRepository extends AbstractRepository {
+    private version;
+    private getVersion;
     count(query: string): Promise<any>;
     execute(query: string, page?: number, limit?: number): Promise<any>;
 }
--- dist/repository.js
+++ dist/repository.js
@@ -1,19 +1,44 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const sdz_agent_types_1 = require("sdz-agent-types");
+const oracleVersion11 = "Oracle Database 11g Release 11.2.0.4.0 - 64bit Production";
 class OracleRepository extends sdz_agent_types_1.AbstractRepository {
+    async getVersion() {
+        const [version] = (await this.execute("SELECT * FROM v$version WHERE banner LIKE '%Oracle%';"))[0];
+    }
     async count(query) {
+        await this.getVersion();
         const total = (await this.execute(`SELECT COUNT (*) as total FROM (${this.buildQuery(query)})`))[0].TOTAL;
         return total;
     }
     execute(query, page, limit) {
-        const statement = [
-            this.buildQuery(query),
-            page && limit ? `OFFSET ${page * limit} ROWS` : null,
-            limit ? `FETCH NEXT ${limit} ROWS ONLY` : null,
-        ]
-            .filter((item) => !!item)
-            .join(" ");
+        let statement;
+        switch (this.version) {
+            case "Oracle Database 11g Release 11.2.0.4.0 - 64bit Production" /* VERSIONS.V11 */:
+                statement = [
+                    this.buildQuery(query),
+                    page && limit
+                        ? `SELECT T.*, rowNum as rowIndex
+          FROM (
+              ${query}
+          )T)T
+          WHERE rowIndex > ${page * limit} AND rowIndex <= ${(page + 1) * limit};`
+                        : null,
+                    limit ? `WHERE rownum <= ${limit}` : null,
+                ]
+                    .filter((item) => !!item)
+                    .join(" ");
+                break;
+            default:
+                statement = [
+                    this.buildQuery(query),
+                    page && limit ? `OFFSET ${page * limit} ROWS` : null,
+                    limit ? `FETCH NEXT ${limit} ROWS ONLY` : null,
+                ]
+                    .filter((item) => !!item)
+                    .join(" ");
+                break;
+        }
         return this.getConnector().execute(statement);
     }
 }
